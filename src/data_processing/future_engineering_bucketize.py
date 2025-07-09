55f265c66d1124c1d2d6a7cb063482cfb5f6b14d
Dominik Jaskuc <flurixoww@gmail.com>
2025-07-09 09:10:14 +0200

    Added first operator

--- src/data_processing/future_engineering_bucketize.py
+++ src/data_processing/future_engineering_bucketize.py
@@ -9,12 +9,18 @@ df = pd.DataFrame({
 
 def bucketize_column(df: pd.DataFrame, col_name: str, categories: list, new_column_name: str, operator: str) -> pd.DataFrame:
     df_copy = df.copy()
-    if df_copy[col_name].dtype not in [np.float64, np.float32]:
-        raise TypeError(f"Column's dtype is not np.float64. \n Note: expected {np.float64}"
+    if df_copy[col_name].dtype not in [np.float64, np.int64]:
+        raise TypeError(f"Column's dtype is not np.float64 or np.int64. \n Note: expected {np.float64} or {np.int64}"
                         f"but got {df_copy[col_name].dtype}", )
     df_copy[new_column_name] = np.nan
-
+    if operator == "==":
+        for i, val in enumerate(df_copy[col_name]):
+            for cat in categories:
+                if val == cat:
+                    df_copy.loc[i, new_column_name] = categories.index(cat)
+                    break
 
     return df_copy
 
 print(bucketize_column(df, "Age", [24, 16], "groups", "=="))
+
